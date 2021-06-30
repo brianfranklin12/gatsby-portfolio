cff53fecd7fc1b30e184990e0e760faf32118416
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-30 11:56:55 -0400

    create postLayout and format gatsby-node to create new pages using the postLayout as component

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,30 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require('path')
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const postLayout = path.resolve('./src/components/postLayout.js')
+  const result = await graphql(`
+  query {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            slug
+          }
+        }
+      }
+    }
+  }
+  
+  `)
+
+  result.data.allMarkdownRemark.edges.forEach(edge => {
+    createPage({
+      path: `posts${edge.node.frontmatter.slug}`,
+      component: postLayout,
+      context: {
+        slug: edge.node.frontmatter.slug
+      }
+    });
+  })
+}
