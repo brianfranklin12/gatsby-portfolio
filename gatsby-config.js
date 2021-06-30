5a6798949116c35ea536ef22b12d2a1a1b1331e0
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-30 08:37:06 -0400

    metadata update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Brian Franklin Portfolio`,
+    description: `A portfolio displaying previous projects done by Brian Franklin.`,
+    author: `Brian Franklin`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
