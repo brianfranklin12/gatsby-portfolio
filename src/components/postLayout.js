3a4b40075b301e88cfc1eb4e7e0e133b33b23efd
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-30 12:16:55 -0400

    posts now render to page

--- src/components/postLayout.js
+++ src/components/postLayout.js
@@ -1,12 +1,33 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 import Layout from './layout'
 
-const postLayout = () => {
+const postLayout = ({ data }) => {
   return (
     <Layout>
-      <h1>Post Layout</h1>
+      <h1>{data.markdownRemark.frontmatter.title}</h1>
+      <div dangerouslySetInnerHTML={{
+        __html: data.markdownRemark.html
+      }}/>
     </Layout>
   )
 }
 
+
+export const query = graphql`
+query PostQuery($slug: String!) {
+  markdownRemark(frontmatter: {
+    slug: {
+      eq: $slug
+    }
+  }) {
+    html
+    frontmatter {
+			title
+      date
+      slug	
+  }
+}
+}
+`
 export default postLayout;
