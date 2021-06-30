76d80f7953432c83abca5fcd74f80382fd0e9cc7
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-30 08:44:01 -0400

    add gatsby-source-filesystem to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    'gatsby-source-filesystem', {
+      options: {
+        path: `${__dirname}/src/posts`,
+        name: 'posts'
+      }
+    }
     `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
