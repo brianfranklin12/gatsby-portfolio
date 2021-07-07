82e59a81f9388cce5fe7c4dd102e9f66f5bdba5f
Brian Franklin <brianfranklinmusic@gmail.com>
2021-07-07 09:33:33 -0400

    font size / color update on banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,6 +8,18 @@ const Container = styled.div`
   width: 100%;
   justify-content: center;
   align-items: center;
+  h1 {
+    font-size: clamp(2.25rem, 8vw, 8rem);
+    font-weight: 600;
+    letter-spacing: 0.1em;
+    margin: 0;
+  }
+  h2 {
+    font-size: clamp(1.125rem, 4vw, 4rem);
+    font-weight: 300;
+    letter-spacing: 0.1em;
+    margin: 1em 0;
+  }
 `
 
 export default function Banner() {
