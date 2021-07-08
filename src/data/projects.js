5d4dffa99f84dc9e8f2f4b9b2194831e7ddfa2cc
Brian Franklin <brianfranklinmusic@gmail.com>
2021-07-08 14:52:42 -0400

    add project descriptions

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,14 +5,14 @@ import spotifyScreen from "../images/spotify-screen.png"
 export const projects = [
   {
     title: "Spotify Music Game",
-    desc: "blah blah blah",
+    desc: "A fun, interactive application that allows a user to test their knowledge of songs in their own playlists. Interacts with Spotify's API for auth, access & refresh tokens, and loading user's playlist information.",
     github: "https://github.com/brianfranklin12/spotify_music_game",
     techStack: spotify,
     img: spotifyScreen
   },
   {
     title: "Coffee Logger",
-    desc: "blah blah blah",
+    desc: "A single-page-application for logging information on your daily coffee brews. Built in vanilla JS, with a Rails API / Postgres DB for the backend.",
     github: "https://github.com/brianfranklin12/coffee_logger",
     techStack: coffeeLogger,
     img: coffeeLoggerScreen
