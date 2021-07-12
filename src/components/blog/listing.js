b8b7ce1ff51fa5d10c2bfbce37b25bc03add5e6c
Brian Franklin <brianfranklinmusic@gmail.com>
2021-07-12 10:59:40 -0400

    add posts route to posts listings page

--- src/components/blog/listing.js
+++ src/components/blog/listing.js
@@ -21,26 +21,43 @@ query BlogPostListing {
   }
 }`
 
+const Header = styled.header`
+  padding: 1rem 3rem;
+  text-align: center;
+  background-color: var(--primary-color);
+  color: var(--bg-color);
+  font-size: 2rem;    
+`
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 2rem auto;
+  width: 80%;
+  max-width: 1000px;
+`
+
 const Post = styled.article`
-  box-shadow: 0px 3px 10px rgba(25, 17, 34, 0.05);
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.2);
   padding: 1rem;
-  border-radius: 4px;
+  border-radius: 8px;
   margin-bottom: 1rem;
   a {
     color: #333;
     text-decoration: none;
   }
   h2 {
+    margin-top: 0;
     margin-bottom: 0;
+    font-size: 2rem;  
   }
   p {
-    font-size: 1rem;
+    font-size: 1.3rem;
   }
   .read-more {
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-    font-size: 1rem;
+    font-size: 1.3rem;
     text-decoration: underline;
-    color: #856084;
+    color: var(--primary-color);
   }
 `
 const Listing = () => {
@@ -48,20 +65,26 @@ const Listing = () => {
   
   return (
     <>
-      {data.allMarkdownRemark.edges.map(({node}) => (
-        <Post key={node.frontmatter.slug}>
-          <Link to={`/posts${node.frontmatter.slug}`}><h2>{node.frontmatter.title}</h2>
-          </Link>
-          <p>{node.frontmatter.date}</p>
-          <p>{node.excerpt}</p>
-          <Link 
-            to={`/posts${node.frontmatter.slug}`}
-            class="read-more"
-          >
-            Read More
-          </Link>
-        </Post>
-      ))}
+      <Header>
+        <h1>Brian Franklin's Blog</h1>
+      </Header>
+      <Container>
+        {data.allMarkdownRemark.edges.map(({node}) => (
+          <Post key={node.frontmatter.slug}>
+            <Link to={`/posts${node.frontmatter.slug}`}><h2>{node.frontmatter.title}</h2>
+            </Link>
+            <p>{node.frontmatter.date}</p>
+            <p>{node.excerpt}</p>
+            <Link 
+              to={`/posts${node.frontmatter.slug}`}
+              class="read-more"
+            >
+              Read More
+            </Link>
+          </Post>
+          
+        ))}
+      </Container>
     </>
   )
 }
---
+++ src/pages/posts.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Listing from '../components/blog/listing';
+
+export default function Posts() {
+  return (
+    <Listing />
+  )
+}
