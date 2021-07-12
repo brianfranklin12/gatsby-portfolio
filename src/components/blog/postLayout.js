0d75f6c13fcf8ba3555f5dd2d7657304904850bd
Brian Franklin <brianfranklinmusic@gmail.com>
2021-07-12 13:57:12 -0400

    styled posts

--- src/components/blog/postLayout.js
+++ src/components/blog/postLayout.js
@@ -2,25 +2,33 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import styled from 'styled-components'
 
+const Container = styled.div`
+  width: 85%;
+  max-width: 1000px;
+  margin: 0 auto;
+`
+
 const Title = styled.h1`
-  letter-spacing: 0.1rem;
+  letter-spacing: 0.2rem;
+  font-size: 3rem;
+  border-bottom: 2px solid var(--primary-color);
 `
 
 const Text = styled.div`
 font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-line-height: 1.6rem;
-font-size: 1.2rem;
+line-height: 1.4em;
+font-size: 2rem;
 `
 
 const postLayout = ({ data }) => {
   const { markdownRemark } = data
   return (
-    <>
+    <Container>
       <Title>{markdownRemark.frontmatter.title}</Title>
       <Text dangerouslySetInnerHTML={{
         __html: markdownRemark.html
       }}/>
-    </>
+    </Container>
   )
 }
 
